4dc2317709b7a6b71f68a7c4edaa7111ad2d934c
Systematix Infotech Pvt. Ltd <dilip_sipl@systematixindia.com>
2020-07-30 16:35:44 +0530

    Added view model file for the widget

---
+++ ViewModels/Widgets/DocumentCategoriesWidgetViewModel.cs
@@ -0,0 +1,11 @@
+namespace DocumentCategoriesWidget.ViewModels.Widgets
+{
+    public class DocumentCategoriesWidgetViewModel
+    {
+        public string DocumentCategories { get; set; }
+
+        public string ContentBefore { get; set; }
+
+        public string ContentAfter { get; set; }
+    }
+}
